583eb6db84f4a59cd3fdc13781b1c34009d2862e
Karol Skolasiński <karolskolasinski@gmail.com>
2020-12-22 18:32:55 +0100

    basic App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+    <div className='wrapper'>
+        <h1 className='main-header'>Hello world</h1>
+        <h1 className='secondary-header'>Hello ELO</h1>
+    </div>
+);
+
+export default App;
